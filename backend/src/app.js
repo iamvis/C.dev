0789d530d125d8f37327adec7b77a270ded0f10d
iamvis <prjapativishal82141@gmail.com>
2025-08-29 15:51:09 +0530

    user model and api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,41 @@
 const express = require("express");
 const app = express();
+const connectDB= require("./config/database");
+const User = require("./models/user");
+
+app.use(express.json())
+
 
 const port = 5000;
 
-app.listen(port, () => {
+app.post("/signup",async(req, res)=>{
+  const userData= req.body;
+  console.log(userData)
+  const newUser= {
+    email:userData.email,
+    password:userData.password
+
+  }
+  try{
+    const user=  new User(userData);
+    await user.save();
+    res.status(201).send(`user creaed SuccessFully ${user}`)
+  }catch(e){
+    res.status(400).send(`Error in User Creation  ${e.message}`)
+
+  }
+  
+
+})
+
+connectDB()
+.then(()=>{ console.log("connection is Established to the DATABASE")
+  app.listen(port, () => {
   console.log(`App is running on port ${port}   http://localhost:5000/`);
 });
+} )
+.catch((e)=>{
+  console.error(e)
+})
+
+
---
+++ backend/src/config/database.js
@@ -0,0 +1,14 @@
+const mongoose= require("mongoose")
+
+const url= `mongodb+srv://prjapativishal82141:4z8v7xD4VPTdedr0@cluster0.0vyofuv.mongodb.net/connectDev
+`
+
+const DBconnect=async()=>{
+    console.log("trying to connect")
+    await mongoose.connect(url )
+     console.log("data base to connect ho jana chahiye")
+}
+
+
+
+module.exports=DBconnect;
