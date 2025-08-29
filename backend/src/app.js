58ae620470f7ebd2f9cfe5a93a389911c1342967
iamvis <prjapativishal82141@gmail.com>
2025-08-29 18:03:49 +0530

    feed api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,41 +1,51 @@
 const express = require("express");
 const app = express();
-const connectDB= require("./config/database");
+const connectDB = require("./config/database");
 const User = require("./models/user");
 
-app.use(express.json())
-
+app.use(express.json());
 
 const port = 5000;
 
-app.post("/signup",async(req, res)=>{
-  const userData= req.body;
-  console.log(userData)
-  const newUser= {
-    email:userData.email,
-    password:userData.password
+app.post("/signup", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    console.log(user);
+    await user.save();
+    res.status(201).send(`user creaed SuccessFully ${user}`);
+  } catch (e) {
+    res.status(400).send(`Error in User Creation  ${e.message}`);
+  }
+});
 
+app.get("/feed", async(req, res)=>{
+  const email= req.body.email;
+  if(!email){
+    res.status(400).send("invalid email");
   }
   try{
-    const user=  new User(userData);
-    await user.save();
-    res.status(201).send(`user creaed SuccessFully ${user}`)
-  }catch(e){
-    res.status(400).send(`Error in User Creation  ${e.message}`)
-
-  }
+     const user=  await User.findOne({email});
+     if(user){
+          res.status(200).send(`user found \n ${user}`);
+     }
+     res.status(400).send("user not found")
+     
   
 
+  }
+  catch(e){
+    res.status(400).send(`Something went wrong \n ${e.message}`)
+
+  }
 })
 
 connectDB()
-.then(()=>{ console.log("connection is Established to the DATABASE")
-  app.listen(port, () => {
-  console.log(`App is running on port ${port}   http://localhost:5000/`);
-});
-} )
-.catch((e)=>{
-  console.error(e)
-})
-
-
+  .then(() => {
+    console.log("connection is Established to the DATABASE");
+    app.listen(port, () => {
+      console.log(`App is running on port ${port}   http://localhost:5000/`);
+    });
+  })
+  .catch((e) => {
+    console.error(e);
+  });
