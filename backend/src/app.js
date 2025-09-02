ba712da33f64acdca1ecc88f03b788598124f19a
iamvis <prjapativishal82141@gmail.com>
2025-09-02 11:32:15 +0530

    all api mthod complete

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,14 +2,20 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
+const port = 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use((req, res, next) => {
+//   console.log("Incoming content-type:", req.headers["content-type"]);
+//   next();
+// });
 
-const port = 5000;
 
 app.post("/signup", async (req, res) => {
   try {
     const user = new User(req.body);
+
     console.log(user);
     await user.save();
     res.status(201).send(`user creaed SuccessFully ${user}`);
@@ -29,13 +35,76 @@ app.get("/feed", async(req, res)=>{
           res.status(200).send(`user found \n ${user}`);
      }
      res.status(400).send("user not found")
-     
-  
 
   }
   catch(e){
     res.status(400).send(`Something went wrong \n ${e.message}`)
 
+  }
+})
+
+//deleteb api
+app.delete("/delete", async (req, res)=>{
+  const userId =  req.body.userid;
+  if(!userId){
+   res.status(400).send("userId not found")  
+  }
+  try{
+    console.log(req.body)
+     const user=await  User.findOneAndDelete({_id: userId});
+     if(user){
+       res.status(200).send("user is succesfuly deleted");
+     }
+     else {
+       res.status(404).send("user not found")  
+  }
+  }
+  catch(e){
+    res.status(400).send( "Something went Wrong", e.message)}
+})
+
+//put api
+app.put("/update", async(req,res)=>{
+    console.log("Raw body:", req.body); // Debugging
+  const userid = req.body.userid;
+  console.log(userid)
+  const data = req.body;
+  
+  if(!data){
+    res.status(400).send("user id  not found");
+  }
+  try{
+    const update =await User.findByIdAndUpdate({_id:userid} , data ,  { new: true, overwrite: true } ) ;
+    if(update ){
+      res.status(200).send("user is fully upadate by put  mehod")
+    }
+    else {
+      res.status(400).send("use updation feild")
+    }
+  }catch{
+     res.status(400).send("something went wrong ", e.message)
+  }
+})
+//patch api
+app.patch("/update-patch", async(req,res)=>{
+    console.log("Raw body:", req.body); // Debugging
+  const userid = req.body.userid;
+  console.log(userid)
+  const data = req.body;
+  
+  if(!data){
+    res.status(400).send("user id  not found");
+  }
+  try{
+    const update =await User.findByIdAndUpdate({_id:userid} , data ,  { new: true, overwrite: true } ) ;
+    if(update ){
+      res.status(200).send("user is fully upadate by patch  mehod")
+    }
+    else {
+      res.status(400).send("use updation feild")
+    }
+  }catch(e){
+     res.status(400).send("something went wrong ", e.message)
   }
 })
 
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -7,59 +7,50 @@ const userSchema = new mongoose.Schema({
     unique: true,
     minlength: 6
   },
-  // username: {
-  //   type: String,
-  //   unique: true,
-  //   required: true,
-  // },
-  // fullName: {
-  //   firstName: {
-  //     type: String,
-  //     required: true
-  //   },
-  //   lastName: {
-  //     type: String
-  //   }
-  // },
+  username: {
+    type: String,
+    unique: true,
+  },
+  fullName: {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String
+    }
+  },
   password: {
     type: String,
-    required: true,
     minlength: 6
   },
-//   age: {
-//     type: Number,
-//     required: true,
-//     min: 0
-//   },
-//   height: {
-//     feet: {
-//       type: Number,
-//       required: true
-//     },
-//     inches: {
-//       type: Number,
-//       required: true
-//     }
-//   },
-//   maritalStatus: {
-//     type: String,
-//     enum: ["married", "single", "divorced", "widowed"],
-//     required: true
-//   },
-//   address: {
-//     type: String,
-//     required: true
-//   },
-//   status: {
-//     type: String,
-//     enum: ["online", "offline"],
-//     default: "offline"
-//   },
-//   dob: {
-//     type: Date,
-//     required: true
-//   }
-// , { timestamps: true }
+  age: {
+    type: Number,
+    min: 0
+  },
+  height: {
+    feet: {
+      type: Number,
+    },
+    inches: {
+      type: Number,
+    }
+  },
+  maritalStatus: {
+    type: String,
+    enum: ["married", "single", "divorced", "widowed"]
+  },
+  address: {
+    type: String
+  },
+  status: {
+    type: String,
+    enum: ["online", "offline"],
+    default: "offline"
+  },
+  dob: {
+    type: Date
+  }
+    // timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
 
 module.exports = mongoose.model("User", userSchema);
