8199ddb9d028b0cef738cb455deb00f94f64f4bd
iamvis <prjapativishal82141@gmail.com>
2025-09-11 18:04:44 +0530

    hash passowrd and login api validation

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
+const { signupValidator, signinValidator } = require("./utils/validator");
+const bcrypt = require("bcrypt");
 const port = 5000;
 
 app.use(express.json());
@@ -11,102 +13,159 @@ app.use(express.urlencoded({ extended: true }));
 //   next();
 // });
 
+////**********************  NEVER TRUST THER USER REQUEST  ***************************** */
 
 app.post("/signup", async (req, res) => {
   try {
-    const user = new User(req.body);
+    // //validation
+    signupValidator(req);
 
-    console.log(user);
-    await user.save();
-    res.status(201).send(`user creaed SuccessFully ${user}`);
+    //destructuring
+    const { email, password, firstName, lastName } = req.body;
+    console.log(email);
+
+    // and save hashed password
+    const user = await User.findOne({ email });
+    if (!user) {
+      //hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+      console.log(hashedPassword);
+      const user = new User({
+        email,
+        password: hashedPassword,
+       fullName:{
+         firstName,
+         lastName,
+       }
+      });
+
+      console.log(user);
+      await user.save();
+      res.status(201).send(`user creaed SuccessFully ${user}`);
+    }
+     else{
+      throw new Error("Invalid Creation")
+     }
+
   } catch (e) {
     res.status(400).send(`Error in User Creation  ${e.message}`);
   }
 });
 
-app.get("/feed", async(req, res)=>{
-  const email= req.body.email;
-  if(!email){
+app.post("/login", async(req, res)=>{
+ try{
+  //validation
+   signinValidator(req);
+    const {email, password}= req.body;
+    
+   //find email in db
+   const user = await User.findOne({email});
+   
+  //if yes
+    if(!user){throw new Error("Invalid Credentials")}
+          //campare hash password
+   const isValidPassword = await bcrypt.compare(password, user.password);
+   if(!isValidPassword){
+    throw new Error("Invalid Credential")
+   }
+
+  //send response
+  res.status(200).send(`user is logged in Succesfully`)
+    
+
+ }
+ catch(error){
+   res.status(400).send("Error : "+error.message)
+ }
+
+})
+
+app.get("/feed", async (req, res) => {
+  const email = req.body.email;
+  if (!email) {
     res.status(400).send("invalid email");
   }
-  try{
-     const user=  await User.findOne({email});
-     if(user){
-          res.status(200).send(`user found \n ${user}`);
-     }
-     res.status(400).send("user not found")
-
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      res.status(200).send(`user found \n ${user}`);
+    }
+    res.status(400).send("user not found");
+  } catch (e) {
+    res.status(400).send(`Something went wrong \n ${e.message}`);
   }
-  catch(e){
-    res.status(400).send(`Something went wrong \n ${e.message}`)
-
-  }
-})
+});
 
 //deleteb api
-app.delete("/delete", async (req, res)=>{
-  const userId =  req.body.userid;
-  if(!userId){
-   res.status(400).send("userId not found")  
+app.delete("/delete", async (req, res) => {
+  const userId = req.body.userid;
+  if (!userId) {
+    res.status(400).send("userId not found");
   }
-  try{
-    console.log(req.body)
-     const user=await  User.findOneAndDelete({_id: userId});
-     if(user){
-       res.status(200).send("user is succesfuly deleted");
-     }
-     else {
-       res.status(404).send("user not found")  
+  try {
+    console.log(req.body);
+    const user = await User.findOneAndDelete({ _id: userId });
+    if (user) {
+      res.status(200).send("user is succesfuly deleted");
+    } else {
+      res.status(404).send("user not found");
+    }
+  } catch (e) {
+    res.status(400).send("Something went Wrong", e.message);
   }
-  }
-  catch(e){
-    res.status(400).send( "Something went Wrong", e.message)}
-})
+});
 
 //put api
-app.put("/update", async(req,res)=>{
-    console.log("Raw body:", req.body); // Debugging
+app.put("/update", async (req, res) => {
+  console.log("Raw body:", req.body); // Debugging
   const userid = req.body.userid;
-  console.log(userid)
+  console.log(userid);
   const data = req.body;
-  
-  if(!data){
+
+  if (!data) {
     res.status(400).send("user id  not found");
   }
-  try{
-    const update =await User.findByIdAndUpdate({_id:userid} , data ,  { new: true, overwrite: true } ) ;
-    if(update ){
-      res.status(200).send("user is fully upadate by put  mehod")
+  try {
+    const update = await User.findByIdAndUpdate({ _id: userid }, data, {
+      new: true,
+      overwrite: true,
+    });
+    if (update) {
+      res.status(200).send("user is fully upadate by put  mehod");
+    } else {
+      res.status(400).send("use updation feild");
     }
-    else {
-      res.status(400).send("use updation feild")
-    }
-  }catch{
-     res.status(400).send("something went wrong ", e.message)
+  } catch {
+    res.status(400).send("something went wrong ", e.message);
   }
-})
+});
 //patch api
-app.patch("/update-patch", async(req,res)=>{
-    console.log("Raw body:", req.body); // Debugging
-  const userid = req.body.userid;
-  console.log(userid)
+app.patch("/update-patch/:id", async (req, res) => {
+  // console.log("Raw body:", req.body); // Debugging
+  const userid = req.params?.id;
+  console.log(userid);
   const data = req.body;
-  
-  if(!data){
+
+  if (!data) {
     res.status(400).send("user id  not found");
   }
-  try{
-    const update =await User.findByIdAndUpdate({_id:userid} , data ,  { new: true, overwrite: true } ) ;
-    if(update ){
-      res.status(200).send("user is fully upadate by patch  mehod")
+  try {
+    if (req.body.email) {
+      throw new Error("Email can not be changed");
     }
-    else {
-      res.status(400).send("use updation feild")
+    const update = await User.findByIdAndUpdate({ _id: userid }, data, {
+      new: true,
+      overwrite: true,
+    });
+    if (update) {
+      res.status(200).send("user is fully upadate by patch  mehod");
+    } else {
+      res.status(400).send("use updation feild");
     }
-  }catch(e){
-     res.status(400).send("something went wrong ", e.message)
+  } catch (e) {
+    res.status(400).send("Error", e.message);
   }
-})
+});
 
 connectDB()
   .then(() => {
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,56 +1,130 @@
 const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    minlength: 6
-  },
-  username: {
-    type: String,
-    unique: true,
-  },
-  fullName: {
-    firstName: {
+const validator = require("validator");
+const userSchema = new mongoose.Schema(
+  {
+    email: {
       type: String,
+      required: true,
+      unique: true,
+      minlength: [6, "email must  have at lease 6 char "],
+      maxLength: [50, "email must be under the 50 char "],
+      lowercase: true,
+      trim: true,
+      //  validate(value){
+      //    if(!validator.isEmail(value)){
+      //     throw new Error("email is invalid")
+      //    }
+      //  }
+      validate: {
+        validator: validator.isEmail,
+        message: "email is inavalid",
+      },
     },
-    lastName: {
-      type: String
-    }
-  },
-  password: {
-    type: String,
-    minlength: 6
-  },
-  age: {
-    type: Number,
-    min: 0
-  },
-  height: {
-    feet: {
-      type: Number,
+    username: {
+      type: String,
+      unique: true,
+      trim: true,
+      minLength: [3, "must have at least 3 char"],
+      maxLength: [20, "must have under 30 char"],
+      lowercase: true,
+      validate: {
+        validator: (value) => /^[a-zA-Z0-9_]+$/.test(value),
+        message: "Username can only contain letters, numbers, and underscores",
+      },
     },
-    inches: {
+    fullName: {
+      firstName: {
+        type: String,
+        trim: true,
+        minLength: [3, "must have at least 3 char"],
+        maxLength: [20, "must have under 30 char"],
+        lowercase: true,
+        // required: true,
+      },
+      lastName: {
+        type: String,
+        trim: true,
+        minLength: [3, "must have at least 3 char"],
+        maxLength: [20, "must have under 30 char"],
+        lowercase: true,
+      },
+    },
+    password: {
+      type: String,
+      minLength: 6,
+      trim: true,
+      required: true,
+      validate: {
+        validator: validator.isStrongPassword,
+        message: "Password should be strong",
+      },
+      maxLength: [200, "must have under 20 char"],
+    },
+    photoURL: {
+      type: String,
+      trim: true,
+      validate: {
+        validator: validator.isURL,
+        message: "Please enter a valid image URL (.jpg, .png, .gif)",
+      },
+    },
+    age: {
       type: Number,
-    }
-  },
-  maritalStatus: {
-    type: String,
-    enum: ["married", "single", "divorced", "widowed"]
-  },
-  address: {
-    type: String
-  },
-  status: {
-    type: String,
-    enum: ["online", "offline"],
-    default: "offline"
-  },
-  dob: {
-    type: Date
-  }
+      min: [18, "must be above 18"],
+      max: [150, "must under 150"],
+      trim: true,
+    },
+    height: {
+      feet: {
+        type: Number,
+        min: [0, "Feet must be a positive number"],
+        max: [9, "Feet must be less than 10"], // optional limit
+      },
+      inches: {
+        type: Number,
+        min: [0, "Inches must be >= 0"],
+        max: [11, "Inches must be <= 11"],
+      },
+    },
+
+    maritalStatus: {
+      type: String,
+      trim: true,
+      enum: ["married", "single", "divorced", "widowed"],
+    },
+    address: {
+      street: { type: String, trim: true },
+      city: { type: String, trim: true },
+      state: { type: String, trim: true },
+      postalCode: {
+        type: String,
+        trim: true,
+        validate: {
+          validator: (val) => /^[0-9]{5,6}$/.test(val), // adjust for country
+          message: "Invalid postal code",
+        },
+      },
+      country: { type: String, trim: true, default: "India" }, // or your default
+    },
+    status: {
+      type: String,
+      enum: ["online", "offline"],
+      default: "offline",
+      trim: true,
+    },
+    dob: {
+      type: Date,
+      trim: true,
+      validate: {
+        validator: (value) => isAdult(18, value),
+        message: "date of Birth can not be future",
+      },
+    },
     // timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
-});
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
